2fce4b389c2f477fe01e93f7a0ae1f92fe18392a
AJ Junaidi <ameerul.junaidi@gmail.com>
2021-07-01 05:12:53 -0400

    Failed attempt to capture data on main page

--- scripts/zipcodes.py
+++ scripts/zipcodes.py
@@ -2,3 +2,5 @@ import pandas as pd
 
 dfZipCodes = pd.read_csv("data/us_states.csv")
 dfZipCodesDict = dfZipCodes.set_index("Representative ZIP Code").T.to_dict("list")
+
+# Source: https://vanwilson.info/2014/11/sample-zip-codes-50-states/
