a82c171fb39d3c075c735ade32a2e0d127d55f49
AJ Junaidi <ameerul.junaidi@gmail.com>
2021-07-03 23:11:01 -0400

    Added check for null dataframes and completed sites

--- scripts/main.py
+++ scripts/main.py
@@ -9,172 +9,185 @@ from selenium.webdriver.support import expected_conditions as EC
 
 from tolist_services import servicesNameCSV, serviceID
 from tolist_zipcodes import zipCodeCSV
-from completed import serviceTodo, zipCodeTodo
+from completed import serviceTodo, zipCodeTodo, completed
 
 PATH = f"{os.getcwd()}/selenium/chromedriver"
 
 serviceList = serviceTodo
 zipCodeList = zipCodeTodo
 
+nullDataFrames = open("src/null_dataframes.txt").read().splitlines()
+
 for serviceName in serviceList:
     for zipCode in zipCodeList:
 
-        # Clear the list for service name and zip codes
-        listServices = []
-        listZipCodes = []
+        if (f"{serviceID[serviceName]}_{zipCode}" in completed) | (
+            f"{serviceID[serviceName]}_{zipCode}" in nullDataFrames
+        ):
+            pass
+        else:
 
-        # Open a chrome page with url specified
-        driver = webdriver.Chrome(PATH)
-        driver.get("https://www.thumbtack.com/more-services")
+            # Clear the list for service name and zip codes
+            listServices = []
+            listZipCodes = []
 
-        # Click the name of the service
-        link = driver.find_element_by_link_text(serviceName)
-        link.click()
+            # Open a chrome page with url specified
+            driver = webdriver.Chrome(PATH)
+            driver.get("https://www.thumbtack.com/more-services")
 
-        # Enter zipcode and press enter
-        enterZipCode = driver.find_element_by_name("zip_code")
-        enterZipCode.send_keys(zipCode)
-        enterZipCode.send_keys(Keys.RETURN)
+            # Click the name of the service
+            link = driver.find_element_by_link_text(serviceName)
+            link.click()
 
-        try:
-            while True:
-                # Make sure the "see more" button is pressed if it's there
-                seeMoreButton = WebDriverWait(driver, 3).until(
-                    EC.presence_of_element_located(
+            # Enter zipcode and press enter
+            enterZipCode = driver.find_element_by_name("zip_code")
+            enterZipCode.send_keys(zipCode)
+            enterZipCode.send_keys(Keys.RETURN)
+
+            try:
+                while True:
+                    # Make sure the "see more" button is pressed if it's there
+                    seeMoreButton = WebDriverWait(driver, 3).until(
+                        EC.presence_of_element_located(
+                            (
+                                By.XPATH,
+                                "//button//span[contains(text(), 'See More')]",
+                            )
+                        )
+                    )
+                    seeMoreButton.click()
+            except:
+                pass
+
+            try:
+                # Get elements for names of services
+                serviceProvider = WebDriverWait(driver, 3).until(
+                    EC.presence_of_all_elements_located(
                         (
                             By.XPATH,
-                            "//button//span[contains(text(), 'See More')]",
+                            "//*[@data-test]/div/div[2]/div/div[1]/div/div[1]/span",
                         )
                     )
                 )
-                seeMoreButton.click()
-        except:
-            pass
+            except:
+                driver.quit()
+                with open("src/null_dataframes.txt", "w") as f:
+                    f.write("%s\n" % f"{serviceID[serviceName]}_{zipCode}")
 
-        try:
-            # Get elements for names of services
-            serviceProvider = WebDriverWait(driver, 3).until(
-                EC.presence_of_all_elements_located(
-                    (
-                        By.XPATH,
-                        "//*[@data-test]/div/div[2]/div/div[1]/div/div[1]/span",
+            try:
+                # Get a list of the names of services
+                listServiceProvider = []
+                for service in serviceProvider:
+                    listServiceProvider.append(service.text)
+
+                # Get elements for ratings (need to get blocks first)
+                blocks = WebDriverWait(driver, 3).until(
+                    EC.presence_of_all_elements_located(
+                        (
+                            By.XPATH,
+                            "//*[@class='flex-1 m_flex relative z-0']",
+                        )
                     )
                 )
-            )
 
-            # Get a list of the names of services
-            listServiceProvider = []
-            for service in serviceProvider:
-                listServiceProvider.append(service.text)
+                # Get children nodes for the blocks for ratings
+                listBlockNodes = []
+                for block in blocks:
+                    listBlockNodes.append(block.find_elements_by_xpath(".//*"))
 
-            # Get elements for ratings (need to get blocks first)
-            blocks = WebDriverWait(driver, 3).until(
-                EC.presence_of_all_elements_located(
-                    (
-                        By.XPATH,
-                        "//*[@class='flex-1 m_flex relative z-0']",
+                # Put all the nodes in a nested list (to make sure NA values are counted)
+                nestedRatings = []
+                for i, listNodes in zip(range(len(listBlockNodes)), listBlockNodes):
+                    nestedRatings.append([])
+                    for nodes in listNodes:
+                        nestedRatings[i].append(nodes.get_attribute("data-star"))
+
+                # Remove all the none values from the children nodes
+                tempRatingsList = []
+                for ratingsList in nestedRatings:
+                    tempRatingsList.append(
+                        [ratings for ratings in ratingsList if ratings is not None]
+                    )
+
+                # Put all ratings into a list
+                listServiceRating = []
+                for ratings in tempRatingsList:
+                    if len(ratings) == 0:
+                        x = NaN
+                    else:
+                        x = ratings[0]
+                    listServiceRating.append(x)
+
+                # Get elements for number of hires
+                serviceHires = WebDriverWait(driver, 3).until(
+                    EC.presence_of_all_elements_located(
+                        (
+                            By.XPATH,
+                            "//*[@data-test]/div/div[2]/div/div[1]/div/div[4]/ul/li[1]/span[2]",
+                        )
                     )
                 )
-            )
 
-            # Get children nodes for the blocks for ratings
-            listBlockNodes = []
-            for block in blocks:
-                listBlockNodes.append(block.find_elements_by_xpath(".//*"))
+                # Put in a temporary list (for # of hires)
+                tempListServiceHires = []
+                for service in serviceHires:
+                    tempListServiceHires.append(service.text)
 
-            # Put all the nodes in a nested list (to make sure NA values are counted)
-            nestedRatings = []
-            for i, listNodes in zip(range(len(listBlockNodes)), listBlockNodes):
-                nestedRatings.append([])
-                for nodes in listNodes:
-                    nestedRatings[i].append(nodes.get_attribute("data-star"))
+                # Check for the word "hire" and put in a list
+                listServiceHires = []
+                for service in tempListServiceHires:
+                    if service.find("hires") != -1:
+                        x = service.split()[0]
+                    else:
+                        x = NaN
+                    listServiceHires.append(x)
 
-            # Remove all the none values from the children nodes
-            tempRatingsList = []
-            for ratingsList in nestedRatings:
-                tempRatingsList.append(
-                    [ratings for ratings in ratingsList if ratings is not None]
-                )
-
-            # Put all ratings into a list
-            listServiceRating = []
-            for ratings in tempRatingsList:
-                if len(ratings) == 0:
-                    x = NaN
-                else:
-                    x = ratings[0]
-                listServiceRating.append(x)
-
-            # Get elements for number of hires
-            serviceHires = WebDriverWait(driver, 3).until(
-                EC.presence_of_all_elements_located(
-                    (
-                        By.XPATH,
-                        "//*[@data-test]/div/div[2]/div/div[1]/div/div[4]/ul/li[1]/span[2]",
+                # Get elements for prices
+                elementServicePrices = WebDriverWait(driver, 3).until(
+                    EC.presence_of_all_elements_located(
+                        (
+                            By.XPATH,
+                            "//*[@data-test]/div/div[2]/div/div[2]",
+                        )
                     )
                 )
-            )
 
-            # Put in a temporary list (for # of hires)
-            tempListServiceHires = []
-            for service in serviceHires:
-                tempListServiceHires.append(service.text)
+                # Put prices into temporary list
+                tempListServicePrice = []
+                for service in elementServicePrices:
+                    tempListServicePrice.append(service.text)
 
-            # Check for the word "hire" and put in a list
-            listServiceHires = []
-            for service in tempListServiceHires:
-                if service.find("hires") != -1:
-                    x = service.split()[0]
-                else:
-                    x = NaN
-                listServiceHires.append(x)
+                # Check for dollar signs and put into list
+                listServicePrice = []
+                for i in tempListServicePrice:
+                    if i.find("$") != -1:
+                        x = i.split()[0]
+                        x = x[1:]
+                    else:
+                        x = NaN
+                    listServicePrice.append(x)
 
-            # Get elements for prices
-            elementServicePrices = WebDriverWait(driver, 3).until(
-                EC.presence_of_all_elements_located(
-                    (
-                        By.XPATH,
-                        "//*[@data-test]/div/div[2]/div/div[2]",
-                    )
+                # Add the name of service and zipcode into a list
+                for i in range(len(listServiceProvider)):
+                    listServices.append(serviceID[serviceName])
+                    listZipCodes.append(zipCode)
+
+                # Create a dictionary for all lists
+                dictServicesProvided = {
+                    "Service": listServiceProvider,
+                    "Type": listServices,
+                    "Rating": listServiceRating,
+                    "Hires": listServiceHires,
+                    "Price": listServicePrice,
+                    "Zip Code": listZipCodes,
+                }
+
+                # Convert to csv for all names and zipcodes
+                dfServicesProvided = pd.DataFrame(dictServicesProvided).to_csv(
+                    f"data/{serviceID[serviceName]}_{zipCode}.csv"
                 )
-            )
+                driver.quit()
 
-            # Put prices into temporary list
-            tempListServicePrice = []
-            for service in elementServicePrices:
-                tempListServicePrice.append(service.text)
-
-            # Check for dollar signs and put into list
-            listServicePrice = []
-            for i in tempListServicePrice:
-                if i.find("$") != -1:
-                    x = i.split()[0]
-                    x = x[1:]
-                else:
-                    x = NaN
-                listServicePrice.append(x)
-
-            # Add the name of service and zipcode into a list
-            for i in range(len(listServiceProvider)):
-                listServices.append(serviceID[serviceName])
-                listZipCodes.append(zipCode)
-
-            # Create a dictionary for all lists
-            dictServicesProvided = {
-                "Service": listServiceProvider,
-                "Type": listServices,
-                "Rating": listServiceRating,
-                "Hires": listServiceHires,
-                "Price": listServicePrice,
-                "Zip Code": listZipCodes,
-            }
-
-            # Convert to csv for all names and zipcodes
-            dfServicesProvided = pd.DataFrame(dictServicesProvided).to_csv(
-                f"data/{serviceID[serviceName]}_{zipCode}.csv"
-            )
-            driver.quit()
-
-        except:
-            driver.quit()
-            pass
+            except:
+                driver.quit()
+                pass
--- tests/test.py
+++ tests/test.py
@@ -1,45 +1,8 @@
-from numpy import nan
+list = [1, 2, 3]
 
-Service = [
-    "M & L CLEANING",
-    "Mon, Jul 5",
-    "Ana's Cleaning Solutions",
-    "Sessoms Tree and Hauling Service",
-    "C & M JANITORIAL CLEANING SERVICES",
-    "CS cleaning solutions",
-]
-Tye = [
-    "House Cleaning",
-    "House Cleaning",
-    "House Cleaning",
-    "House Cleaning",
-    "House Cleaning",
-    "House Cleaning",
-]
-Rating = ["4.5", "5", "5", nan, "4.5"]
-Hires = ["27", "122", "23", nan, "10"]
-ZipCodes = ["95814", "95814", "95814", "95814", "95814", "95814"]
+with open("src/null_dataframes.txt", "w") as f:
+    for item in list:
+        f.write("%s\n" % item)
 
-Service = [
-    "Avery's Enhanced Cleaning",
-    "Wayuu Organic Cleaning",
-    "Sun, Jul 11",
-    "Ammana's House Cleanser",
-    "Ishmael Seven Jasso",
-    "Karen Hernandez",
-    "house Cleaning",
-    "Matthew Environmental Janitorial",
-]
-Tye = [
-    "House Cleaning",
-    "House Cleaning",
-    "House Cleaning",
-    "House Cleaning",
-    "House Cleaning",
-    "House Cleaning",
-    "House Cleaning",
-    "House Cleaning",
-]
-Rating = ["5", "5", "4.5", nan, nan, "4.5", "5"]
-Hires = [nan, "45", "168", nan, nan, "5", "7"]
-ZipCodes = ["90013", "90013", "90013", "90013", "90013", "90013", "90013", "90013"]
+
+nullDataFrames = open("src/null_dataframes.txt").read().splitlines()
